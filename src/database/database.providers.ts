0b616725b708c31aa609905fe56f9c463a62a7c9
Honor <admin@honor2045.com>
2022-07-06 20:15:05 -0300

    feat(database): add sequelize for mysql

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,1 +1,21 @@
-export const databaseProviders = [];
+import { Sequelize } from 'sequelize-typescript';
+
+export const databaseProviders = [
+    {
+        provide: 'SEQUELIZE',
+        useFactory: async () => {
+            const sequelize = new Sequelize({
+                dialect: 'mysql',
+                host: process.env.HOST,
+                port: parseInt(process.env.PORT),
+                username: process.env.USERNAME,
+                password: process.env.PASSWORD,
+                database: 'nest',
+                logging: false,
+            });
+
+            await sequelize.sync();
+            return sequelize;
+        },
+    },
+];
