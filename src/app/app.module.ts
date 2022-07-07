ce8cb0b061971d8529d8c7ca4dd8721424eadab9
Honor <admin@honor2045.com>
2022-07-06 21:24:50 -0300

    feat(app): add backup feat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ApiModule } from 'src/api/api.module';
+import { BackupModule } from 'src/backup/backup.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
         ApiModule,
+        BackupModule,
+        ScheduleModule.forRoot(),
     ],
 })
 export class AppModule {}
